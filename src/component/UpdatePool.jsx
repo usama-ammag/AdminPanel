842067b9a68073229ead8cf16e9cb85c3868d82d
usama-ammag <89912457+usama-ammag@users.noreply.github.com>
2021-11-01 19:34:55 +0500

    updated code

--- src/component/UpdatePool.jsx
+++ src/component/UpdatePool.jsx
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { SendHttpRequest } from "./utility";
+import axios from "axios";
+import {
+  BaseUrl,
+  AuthenticationTokenId,
+  ImageBaseUrl,
+  BaseUrlGet,
+} from "../Constants/BusinessManager";
 
 const UpdatePool = () => {
   const [data, setData] = useState();
-  useEffect(() => {}, []);
   const { id } = useParams();
+  useEffect(() => {
+    getProject(id);
+  }, []);
+
+  function getProject(Id) {
+    axios
+      .get(BaseUrl + `v1/Pool/GetPoolById?ProjectId=${Id}`)
+      .then((resp) => {
+        setData(resp.data.data);
+        // console.log(resp.data.data)
+      })
+      .catch((error) => {});
+  }
+
   return (
     <div>
       <div className="form-group row">
@@ -17,6 +38,7 @@ const UpdatePool = () => {
             Full Name
           </label>
           <input
+            defaultValue={data?.fullName}
             type="text"
             name="fullName"
             className="form-control"
@@ -32,6 +54,7 @@ const UpdatePool = () => {
             Title
           </label>
           <input
+            defaultValue={data?.title}
             type="text"
             name="title"
             className="form-control"
@@ -49,6 +72,7 @@ const UpdatePool = () => {
             Telegram
           </label>
           <input
+            defaultValue={data?.telegram}
             type="text"
             name="telegram"
             className="form-control"
@@ -64,6 +88,7 @@ const UpdatePool = () => {
             Email
           </label>
           <input
+            defaultValue={data?.email}
             type="text"
             name="email"
             className="form-control"
@@ -81,6 +106,7 @@ const UpdatePool = () => {
             Project Name
           </label>
           <input
+            defaultValue={data?.projectName}
             type="text"
             name="projectName"
             className="form-control"
@@ -96,6 +122,7 @@ const UpdatePool = () => {
             Website Url
           </label>
           <input
+            defaultValue={data?.websiteURL}
             type="text"
             name="websiteUrl"
             className="form-control"
@@ -113,6 +140,7 @@ const UpdatePool = () => {
             Whitepaper Url
           </label>
           <input
+            defaultValue={data?.whitepaperURL}
             type="text"
             name="whitepaperUrl"
             className="form-control"
@@ -128,6 +156,7 @@ const UpdatePool = () => {
             Link to Desk
           </label>
           <input
+            defaultValue={data?.linktoDeck}
             type="text"
             name="linkToDesk"
             className="form-control"
@@ -145,6 +174,7 @@ const UpdatePool = () => {
             Project Twitter
           </label>
           <input
+            defaultValue={data?.projectTwitter}
             type="text"
             name="projectTwitter"
             className="form-control"
@@ -160,6 +190,7 @@ const UpdatePool = () => {
             Project Telegram
           </label>
           <input
+            defaultValue={data?.projectTelegram}
             type="text"
             name="projectTelegram"
             className="form-control"
@@ -177,6 +208,7 @@ const UpdatePool = () => {
             Project Github
           </label>
           <input
+            defaultValue={data?.projectGithub}
             type="text"
             name="projectGithub"
             className="form-control"
@@ -189,9 +221,10 @@ const UpdatePool = () => {
             className="col-form-label TEXTFIELD_FIRST"
             style={{ fontWeight: "bold", textTransform: "uppercase" }}
           >
-           Project Description
+            Project Description
           </label>
-          <input
+          <textarea
+            defaultValue={data?.projectDescription}
             type="text"
             name="projectDescription"
             className="form-control"
@@ -200,9 +233,6 @@ const UpdatePool = () => {
         </div>
       </div>
 
-
-
-
       <div className="form-group row">
         <div className="col-sm-6">
           <label
@@ -210,9 +240,10 @@ const UpdatePool = () => {
             className="col-form-label TEXTFIELD_FIRST"
             style={{ fontWeight: "bold", textTransform: "uppercase" }}
           >
-          Token Information
+            Token Information
           </label>
           <input
+            defaultValue={data?.tokenInformation}
             type="text"
             name="tokenInformation"
             className="form-control"
@@ -225,9 +256,10 @@ const UpdatePool = () => {
             className="col-form-label TEXTFIELD_FIRST"
             style={{ fontWeight: "bold", textTransform: "uppercase" }}
           >
-           Product Development State
+            Product Development State
           </label>
           <input
+            defaultValue={data?.productDevelopmentState}
             type="text"
             name="productDevelopmentState"
             className="form-control"
@@ -236,8 +268,6 @@ const UpdatePool = () => {
         </div>
       </div>
 
-
-
       <div className="form-group row">
         <div className="col-sm-6">
           <label
@@ -245,9 +275,10 @@ const UpdatePool = () => {
             className="col-form-label TEXTFIELD_FIRST"
             style={{ fontWeight: "bold", textTransform: "uppercase" }}
           >
-           Development Roadmap
+            Development Roadmap
           </label>
           <input
+            defaultValue={data?.developmentRoadmap}
             type="text"
             name="developmentRoadmap"
             className="form-control"
@@ -260,9 +291,10 @@ const UpdatePool = () => {
             className="col-form-label TEXTFIELD_FIRST"
             style={{ fontWeight: "bold", textTransform: "uppercase" }}
           >
-           Unique Value Proposition
+            Unique Value Proposition
           </label>
           <input
+            defaultValue={data?.uniqueValueProposition}
             type="text"
             name="uniqueValueProposition"
             className="form-control"
@@ -270,8 +302,6 @@ const UpdatePool = () => {
           />
         </div>
       </div>
-
-
 
       <div className="form-group row">
         <div className="col-sm-6">
@@ -283,6 +313,7 @@ const UpdatePool = () => {
             Cardono Contribution
           </label>
           <input
+            defaultValue={data?.cardonoContribution}
             type="text"
             name="cardonoContribution"
             className="form-control"
@@ -295,9 +326,10 @@ const UpdatePool = () => {
             className="col-form-label TEXTFIELD_FIRST"
             style={{ fontWeight: "bold", textTransform: "uppercase" }}
           >
-           Project Financing Structure
+            Project Financing Structure
           </label>
           <input
+            defaultValue={data?.projectFinancingStructure}
             type="text"
             name="projectFinancingStructure"
             className="form-control"
@@ -306,17 +338,268 @@ const UpdatePool = () => {
         </div>
       </div>
 
+      <div className="form-group row">
+        <div className="col-sm-6">
+          <label
+            htmlFor="blockchainName"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Blockchain Name
+          </label>
+          <input
+            defaultValue={data?.blockChainName}
+            type="text"
+            name="blockchainName"
+            className="form-control"
+            placeholder="Blockchain Name"
+          />
+        </div>
+        <div className="col-sm-6">
+          <label
+            htmlFor="contractAddres"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Contract Addres
+          </label>
+          <input
+            defaultValue={data?.contractAddress}
+            type="text"
+            name="contractAddres"
+            className="form-control"
+            placeholder="Contract Addres"
+          />
+        </div>
+      </div>
 
+      <div className="form-group row">
+        <div className="col-sm-6">
+          <label
+            htmlFor="shortDescription"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Short Description
+          </label>
+          <input
+            defaultValue={data?.shortDescription}
+            type="text"
+            name="shortDescription"
+            className="form-control"
+            placeholder="Short Description"
+          />
+        </div>
+        <div className="col-sm-6">
+          <label
+            htmlFor="raiseTarget"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Raise Target (usd)
+          </label>
+          <input
+            defaultValue={data?.raiseTarget}
+            type="number"
+            type="text"
+            name="raiseTarget"
+            className="form-control"
+            placeholder="Raise Target (usd)"
+          />
+        </div>
+      </div>
 
+      <div className="form-group row">
+        <div className="col-sm-6">
+          <label
+            htmlFor="expectedLaunchDate"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Expected Launch Date
+          </label>
+          <input
+            defaultValue={data?.expectedLaunchDate.split("T")[0]}
+            type="text"
+            type="date"
+            name="expectedLaunchDate"
+            className="form-control"
+            placeholder="Expected Launch Date"
+          />
+        </div>
+        <div className="col-sm-6">
+          <label
+            htmlFor="listingStatus"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Listing Status
+          </label>
+          <input
+            // defaultValue={data?.fullName}
+            type="text"
+            name="listingStatus"
+            className="form-control"
+            placeholder="Listing Status"
+          />
+        </div>
+      </div>
 
+      <div className="form-group row">
+        <div className="col-sm-6">
+          <label
+            htmlFor="supplyAmount"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Supply Amount
+          </label>
+          <input
+            defaultValue={data?.supplyAmount}
+            type="number"
+            type="text"
+            name="supplyAmount"
+            className="form-control"
+            placeholder="Supply Amount"
+          />
+        </div>
+        <div className="col-sm-6">
+          <label
+            htmlFor="tokenRate"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Token Rate (usd)
+          </label>
+          <input
+            defaultValue={data?.tokenRate}
+            type="text"
+            type="number"
+            name="tokenRate"
+            className="form-control"
+            placeholder="Token Rate (usd)"
+          />
+        </div>
+      </div>
 
+      <div className="form-group row">
+        <div className="col-sm-6">
+          <label
+            htmlFor="supplyTransactionHash"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Supply TransactionHash
+          </label>
+          <input
+            // defaultValue={data?.fullName}
+            type="text"
+            name="supplyTransactionHash"
+            className="form-control"
+            placeholder="Supply TransactionHash"
+          />
+        </div>
+        <div className="col-sm-6">
+          <label
+            htmlFor="supplyTransactionHash"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Supply Transaction Status
+          </label>
+          <input
+            defaultValue={data?.supplyTransactionStatus}
+            type="text"
+            name="supplyTransactionHash"
+            className="form-control"
+            placeholder="Supply Transaction Status"
+          />
+        </div>
+      </div>
 
+      <div className="form-group row">
+        <div className="col-sm-6">
+          <label
+            htmlFor="remainingSupply"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Remaining Supply
+          </label>
+          <input
+            defaultValue={data?.remainingSupplyAmount}
+            type="number"
+            name="remainingSupply"
+            className="form-control"
+            placeholder="Remaining Supply"
+          />
+        </div>
+        <div className="col-sm-6">
+          <label
+            htmlFor="pendingSupplyDeposit"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Pending Supply deposit
+          </label>
+          <input
+            // defaultValue={data?.fullName}
+            type="number"
+            name="pendingSupplyDeposit"
+            className="form-control"
+            placeholder="Pending Supply deposit"
+          />
+        </div>
+      </div>
 
-
-
-
-
-
+      <div className="form-group row">
+        <div className="col-sm-6">
+          <label
+            htmlFor="confirmedSupplyDeposited"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Confirmed Supply Deposited
+          </label>
+          <input
+            // defaultValue={data?.fullName}
+            type="number"
+            name="confirmedSupplyDeposited"
+            className="form-control"
+            placeholder="Confirmed Supply Deposited"
+          />
+        </div>
+        <div className="col-sm-6">
+          <label
+            htmlFor="createdAt"
+            className="col-form-label TEXTFIELD_FIRST"
+            style={{ fontWeight: "bold", textTransform: "uppercase" }}
+          >
+            Created At
+          </label>
+          <input
+            // defaultValue={data?.fullName}
+            type="date"
+            name="createdAt"
+            className="form-control"
+            placeholder="Created At"
+          />
+        </div>
+      </div>
+      <div style={{textAlign:"center",margin:"30px 0px"}}>
+      <button
+        type="submit"
+        className="create-btn"
+        style={{
+          fontWeight: "bold",
+          letterSpacing: 1,
+          width: "30%",
+        
+        }}
+      >
+        Update
+      </button>
+      </div>
     </div>
   );
 };
